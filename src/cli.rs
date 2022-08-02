ab7518e2269473c85908ece6bb7261c1a2041d12
Nicholas Orta <nickorta12@gmail.com>
2022-08-01 22:08:20 -0600

    Initial commit

---
+++ src/cli.rs
@@ -0,0 +1,52 @@
+use std::str::FromStr;
+
+use clap::Parser;
+use clap_verbosity_flag::Verbosity;
+use url::Url;
+
+#[derive(Debug)]
+pub struct TryUrl(Url);
+
+fn validate_fragment(frag: &str) -> bool {
+    match frag.split_once('/') {
+        Some((parent, repo)) => !(parent.is_empty() || repo.is_empty()),
+        None => false,
+    }
+}
+
+impl FromStr for TryUrl {
+    type Err = url::ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match Url::parse(s) {
+            Ok(url) => Ok(Self(url)),
+            Err(_) if validate_fragment(s) => {
+                Url::parse(&format!("https://www.github.com/{}", s)).map(|x| x.into())
+            }
+            Err(err) => Err(err),
+        }
+    }
+}
+
+impl From<Url> for TryUrl {
+    fn from(url: Url) -> Self {
+        Self(url)
+    }
+}
+
+impl From<TryUrl> for Url {
+    fn from(try_url: TryUrl) -> Self {
+        try_url.0
+    }
+}
+
+#[derive(Debug, Parser)]
+pub struct Cli {
+    #[clap(flatten)]
+    pub verbose: Verbosity,
+    pub url: TryUrl,
+}
+
+pub fn args() -> Cli {
+    Cli::parse()
+}
---
+++ src/main.rs
@@ -0,0 +1,216 @@
+use std::{error, fmt, fs, path::Path, process::Command};
+
+use directories::{BaseDirs, ProjectDirs};
+use log::debug;
+use tokei::{LanguageType, Languages};
+use url::{Host, Url};
+
+mod cli;
+
+fn main() -> eyre::Result<()> {
+    let cli = cli::args();
+    color_eyre::install()?;
+    env_logger::builder()
+        .filter_level(cli.verbose.log_level_filter())
+        .init();
+
+    let project_dirs = ProjectDirs::from("", "", "gclone").unwrap();
+    let base_dirs = BaseDirs::new().unwrap();
+    let url: Url = cli.url.into();
+    debug!("URL: {:#?}", url);
+
+    let location = RepoLocation::from_url(url)?;
+    debug!("Location: {:?}", location);
+
+    let parent_dir = project_dirs
+        .cache_dir()
+        .join(location.host)
+        .join(location.parent);
+    let repo_dir = parent_dir.join(location.repo);
+    let symlink_dir = base_dirs.home_dir().join("repos");
+    if repo_dir.exists() {
+        if let Ok(true) =
+            inquire::Confirm::new(&format!("{} already exists, reclone?", repo_dir.display()))
+                .with_default(false)
+                .prompt()
+        {
+            fs::remove_dir(&repo_dir)?;
+        } else {
+            symlink(&repo_dir, &symlink_dir)?;
+            return Ok(());
+        }
+    }
+    fs::create_dir_all(&parent_dir)?;
+    clone_repo(&location.url, &parent_dir, &repo_dir)?;
+    symlink(&repo_dir, &symlink_dir)?;
+
+    Ok(())
+}
+
+fn clone_repo(url: &Url, parent_dir: &Path, repo_dir: &Path) -> eyre::Result<()> {
+    println!("Cloning {} to {}", url.as_str(), repo_dir.display());
+    let status = Command::new("git")
+        .arg("clone")
+        .arg(url.as_str())
+        .arg(repo_dir)
+        .current_dir(parent_dir)
+        .status()?;
+
+    if status.success() {
+        Ok(())
+    } else {
+        Err(eyre::eyre!("git clone command failed"))
+    }
+}
+
+fn get_top_language(repo_dir: &Path) -> String {
+    let mut languages = Languages::new();
+    languages.get_statistics(&[repo_dir], &[], &Default::default());
+
+    let mut top_language_fallback: Option<LanguageType> = None;
+    let mut top_fallback_code = 0;
+
+    let mut top_language_type: Option<LanguageType> = None;
+    let mut top_code = 0;
+    for (language_type, language) in languages {
+        debug!("Found language: {language_type}");
+        let code = language.summarise().code;
+        if code > top_code {
+            if matches!(
+                language_type,
+                LanguageType::Json
+                    | LanguageType::Yaml
+                    | LanguageType::Html
+                    | LanguageType::Css
+                    | LanguageType::Markdown
+            ) && code > top_fallback_code
+            {
+                top_language_fallback = Some(language_type);
+                top_fallback_code = code;
+            } else {
+                top_language_type = Some(language_type);
+                top_code = code;
+            }
+        }
+    }
+
+    if top_language_type.is_none() && top_language_fallback.is_some() {
+        top_language_type = top_language_fallback;
+    }
+
+    top_language_type.map_or("other".to_string(), |t| {
+        t.to_string()
+            .to_lowercase()
+            .replace(' ', "")
+            .replace('_', "-")
+            .replace("c#", "csharp")
+    })
+}
+
+fn symlink(repo_dir: &Path, symlink_dir: &Path) -> eyre::Result<()> {
+    if !symlink_dir.exists() {
+        fs::create_dir(&symlink_dir)?;
+    }
+    let lang = get_top_language(repo_dir);
+    let lang_dir = symlink_dir.join(lang);
+    if !lang_dir.exists() {
+        fs::create_dir(&lang_dir)?;
+    }
+
+    let dest_dir = lang_dir.join(
+        repo_dir
+            .file_name()
+            .ok_or(eyre::eyre!("unable to get file name of repo"))?,
+    );
+    if dest_dir.exists() {
+        println!(
+            "Repo {} already linked at {}",
+            repo_dir.display(),
+            dest_dir.display()
+        );
+        return Ok(());
+    }
+    println!("Linking {} to {}", repo_dir.display(), dest_dir.display());
+    std::os::unix::fs::symlink(repo_dir, &dest_dir)?;
+
+    Ok(())
+}
+
+#[derive(Debug, Clone)]
+enum LocationPart {
+    Parent,
+    Repo,
+    InvalidHost,
+}
+
+#[derive(Debug, Clone)]
+struct LocationParsingError(LocationPart);
+
+impl error::Error for LocationParsingError {}
+
+impl fmt::Display for LocationParsingError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            LocationPart::Parent => f.write_str("unable to parse parent portion of url"),
+            LocationPart::Repo => f.write_str("unable to parse repo portion of url"),
+            LocationPart::InvalidHost => f.write_str("invalid hostname"),
+        }
+    }
+}
+
+impl LocationParsingError {
+    fn parent() -> Self {
+        Self(LocationPart::Parent)
+    }
+
+    fn repo() -> Self {
+        Self(LocationPart::Repo)
+    }
+
+    fn host() -> Self {
+        Self(LocationPart::InvalidHost)
+    }
+}
+
+#[derive(Debug)]
+struct RepoLocation {
+    url: Url,
+    parent: String,
+    repo: String,
+    host: String,
+}
+
+impl RepoLocation {
+    fn from_url(url: Url) -> Result<Self, LocationParsingError> {
+        let mut segments = url
+            .path_segments()
+            .ok_or_else(LocationParsingError::parent)?;
+
+        struct Partial {
+            parent: String,
+            repo: String,
+        }
+
+        let partial = match segments.next() {
+            Some(parent) => match segments.next() {
+                Some(repo) => Ok(Partial {
+                    parent: parent.to_owned(),
+                    repo: repo.to_owned(),
+                }),
+                None => Err(LocationParsingError::repo()),
+            },
+            None => Err(LocationParsingError::parent()),
+        }?;
+
+        match url.host() {
+            Some(Host::Domain(host)) => Ok(Self {
+                parent: partial.parent,
+                repo: partial.repo,
+                host: host.replace("www.", ""),
+                url,
+            }),
+            Some(_) => Err(LocationParsingError::host()),
+            None => Err(LocationParsingError::host()),
+        }
+    }
+}
